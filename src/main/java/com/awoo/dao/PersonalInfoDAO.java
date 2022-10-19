6bad7afe91569c97914d3466e936369a60b8d0fb
y2tkfkd <y2tkfkd@naver.com>
2022-10-19 12:44:14 +0900

    강혜은 - personalInfoDAO 수정 (22.10.19)

--- src/main/java/com/awoo/dao/PersonalInfoDAO.java
+++ src/main/java/com/awoo/dao/PersonalInfoDAO.java
@@ -1,5 +1,15 @@
     package com.awoo.dao;
 
-public interface PersonalInfoDAO {
+import java.util.List;
 
+import org.apache.ibatis.annotations.Mapper;
+
+import com.awoo.vo.PersonalInfoVO;
+
+@Mapper
+public interface PersonalInfoDAO {
+	
+	public List<PersonalInfoVO> selectPersonal();
+
+	public List<PersonalInfoVO> selectAllInfo();
 }
